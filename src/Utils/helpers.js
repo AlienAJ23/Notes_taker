58196d4f9d38092d3fd71d4c44ef32cad949fe1d
Akshat Jain <88207070+AlienAJ23@users.noreply.github.com>
2025-07-21 02:44:00 +0530

    Update helpers.js

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -1,7 +1,7 @@
 
 
 export function sortNotes(notes) {
-  // First pinned notes (by updated time desc), then unpinned (by updated time desc)
+  // First pinned notes (by updated time in descending order), then unpinned (by updated time in descending order)
   return [...notes]
     .sort((a, b) => Number(b.pinned) - Number(a.pinned) || new Date(b.updatedAt) - new Date(a.updatedAt));
 }
